e61be7e173aa720c814735291ce6df570483fbea
Ant <anttears@hotmnail.com>
2020-09-13 22:46:32 +0100

    Update for AWS

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
-'use strict';
+const https = require('https');
+const fs = require('fs');
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const fileupload = require('express-fileupload');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const expressSession = require('express-session')({
+    secret: process.env.secret,
+    resave: false,
+    saveUninitialized: false
+});
 
-var fs = require('fs');
-var path = require('path');
+const apiRouter = require('./routes/api');
 
-exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
-  var result = {
-    statusCode: 200,
-    body: contents.toString(),
-    headers: {'content-type': 'text/html'}
-  };
+const app = express();
 
-  callback(null, result);
-};
+app.use(express.json({limit: '300kb'}));
+app.use(expressSession);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(fileupload());
+app.use(xss());
+app.use(mongoSanitize());
+app.use(helmet());
+const limit = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests'
+});
+
+app.use('/api', limit, apiRouter);
+
+const mongoDB = process.env.mongoDB;
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.on('error', console.error.bind(console, 'Database connection error: '));
+
+https.createServer({
+    key: fs.readFileSync(path.join(__dirname, './key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, './cert.pem'))
+}, app).listen(process.env.PORT);
